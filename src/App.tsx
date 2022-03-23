b7e549b8e97c46283bc46fbaa33c7070b0a1d5bf
황동현 <dev_donghyun@kakao.com>
2022-03-23 17:41:18 +0900

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
+import QRCode from "react-qr-code";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData(event.target.value);
+  };
+
+  const handleOnClickDownloadBtn = () => {
+    const svg = document.getElementById("qr-code") as HTMLElement;
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx?.drawImage(image, 0, 0);
+      const pngFile = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.download = "QRCode";
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    image.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <QRCode id="qr-code" value={data} />
+      <form className="data-form">
+        <input type="text" value={data} onChange={handleInputChange} />
+      </form>
+      <button
+        type="button"
+        className="btn-download"
+        onClick={handleOnClickDownloadBtn}
+      >
+        Download to image
+      </button>
     </div>
   );
-}
+};
 
 export default App;
